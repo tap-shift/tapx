6e541a9cf23afa410592b3d3ecd19c279d8f66dc
tapshift <141554865+tap-shift@users.noreply.github.com>
2025-07-25 18:13:21 +0200

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,1 +1,7 @@
-
+return {
+    ["TAPX-ABCD-1234"] = {type = "eb"},
+    ["TAPX-EFGH-5678"] = {type = "eu"},
+    ["TAPX-IJKL-9012"] = {type = "pro"},
+    ["TAPX-MNOP-3456"] = {type = "eb"},
+    ["TAPX-QRST-7890"] = {type = "pro"}
+}
