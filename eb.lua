eed52e98bbd69600432538852559e851d489ae2b
tapshift <141554865+tap-shift@users.noreply.github.com>
2025-07-25 18:31:47 +0200

    Update eb.lua

--- eb.lua
+++ eb.lua
@@ -1,7 +1,14 @@
 return {
-    users = {
-        "Player1",
-        "Player2",
-        "Player3"
+    {
+        key = "TAPX-EB-1111",
+        users = {"Player1", "Player2", "Player3"}
+    },
+    {
+        key = "TAPX-EB-2222",
+        users = {"Player4", "Player5", "Player6"}
+    },
+    {
+        key = "TAPX-EB-3333",
+        users = {"Player7", "Player8", "Player9"}
     }
 }
