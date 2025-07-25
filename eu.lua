ca39802e16d5dac56c1e61b60a4f74a0ba195974
tapshift <141554865+tap-shift@users.noreply.github.com>
2025-07-25 18:31:57 +0200

    Update eu.lua

--- eu.lua
+++ eu.lua
@@ -1,7 +1,14 @@
 return {
-    users = {
-        "Player4",
-        "Player5",
-        "Player6"
+    {
+        key = "TAPX-EU-4444",
+        users = {"User1", "User2", "User3"}
+    },
+    {
+        key = "TAPX-EU-5555",
+        users = {"User4", "User5", "User6"}
+    },
+    {
+        key = "TAPX-EU-6666",
+        users = {"User7", "User8", "User9"}
     }
 }
