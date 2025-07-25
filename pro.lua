2c41d3973194024a7854b0de2bd6ce7d8cc3a88f
tapshift <141554865+tap-shift@users.noreply.github.com>
2025-07-25 18:32:15 +0200

    Update pro.lua

--- pro.lua
+++ pro.lua
@@ -1,7 +1,14 @@
 return {
-    users = {
-        "Player7",
-        "Player8",
-        "Player9"
+    {
+        key = "TAPX-PRO-7777",
+        users = {"ProUser1"}
+    },
+    {
+        key = "TAPX-PRO-8888",
+        users = {"ProUser2"}
+    },
+    {
+        key = "TAPX-PRO-9999",
+        users = {"ProUser3"}
     }
 }
