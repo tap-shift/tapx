481b045933f99cdb59a74439ceb84426c7353839
tapshift <141554865+tap-shift@users.noreply.github.com>
2025-07-27 16:02:15 +0200

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -1,11 +1,64 @@
 return {
-    ["TAPX-EB-1111"] = {type = "eb"},
-    ["TAPX-EB-2222"] = {type = "eb"},
-    ["TAPX-EB-3333"] = {type = "eb"},
-    ["TAPX-EU-4444"] = {type = "eu"},
-    ["TAPX-EU-5555"] = {type = "eu"},
-    ["TAPX-EU-6666"] = {type = "eu"},
-    ["TAPX-PRO-7777"] = {type = "pro"},
-    ["TAPX-PRO-8888"] = {type = "pro"},
-    ["TAPX-PRO-9999"] = {type = "pro"}
+    keys = {
+        -- EB KEYS
+        ["TAPX-EB-1111"] = {type = "eb"},
+        ["TAPX-EB-2222"] = {type = "eb"},
+        ["TAPX-EB-3333"] = {type = "eb"},
+        -- EU KEYS
+        ["TAPX-EU-4444"] = {type = "eu"},
+        ["TAPX-EU-5555"] = {type = "eu"},
+        ["TAPX-EU-6666"] = {type = "eu"},
+        -- PRO KEYS
+        ["TAPX-PRO-7777"] = {type = "pro"},
+        ["TAPX-PRO-8888"] = {type = "pro"},
+        ["TAPX-PRO-9999"] = {type = "pro"}
+    },
+    
+    -- Pro keys data
+    pro = {
+        {
+            key = "TAPX-PRO-7777",
+            users = {"urskid11"}
+        },
+        {
+            key = "TAPX-PRO-8888",
+            users = {"rivalenMeisterPro"}
+        },
+        {
+            key = "TAPX-PRO-9999",
+            users = {"Steven1234wth2"}
+        }
+    },
+    
+    -- EB keys data
+    eb = {
+        {
+            key = "TAPX-EB-1111",
+            users = {"Player1", "Player2", "Player3"}
+        },
+        {
+            key = "TAPX-EB-2222",
+            users = {"Player4", "Player5", "Player6"}
+        },
+        {
+            key = "TAPX-EB-3333",
+            users = {"Player7", "Player8", "Player9"}
+        }
+    },
+    
+    -- EU keys data
+    eu = {
+        {
+            key = "TAPX-EU-4444",
+            users = {"User1", "User2", "User3"}
+        },
+        {
+            key = "TAPX-EU-5555",
+            users = {"User4", "User5", "User6"}
+        },
+        {
+            key = "TAPX-EU-6666",
+            users = {"User7", "User8", "User9"}
+        }
+    }
 }
