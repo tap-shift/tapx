337d7ce6496e5a2545258bc6815f9f8e737bc458
tapshift <141554865+tap-shift@users.noreply.github.com>
2025-07-31 18:24:27 +0200

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1,21 +1,63 @@
 --!strict
 local Bypasser = {}
 
-function Bypasser.bypassText(text: string): string
-	local result = {}
+-- Homoglyphen-Tabelle
+local charSubstitutions = {
+	a = { "ą", "ḁ", "aธ", "aั", "ᴀ", "ส", "ạ", "à" },
+	b = { "ƀ", "ḅ", "bม", "ɓ", "฿" },
+	c = { "cׂׂׂ", "ç", "cׂꞣ", "cׂ", "ͼ", "¢" },
+	d = { "đ", "dִִִ", "ď", "ḓ", "ḍ", "ɗ" },
+	e = { "ė", "ề", "eִִִ", "ɛ", "є", "ẹ" },
+	f = { "fม", "ƒ", "ғ", "fͼ", "f͟" },
+	g = { "ǥ", "gִ", "ɠ", "ģ" },
+	h = { "ĥ", "ћ", "hִִִ", "ḥ", "ђ", "Ħ" },
+	i = { "į", "iͼ", "ί", "ỉ" },
+	j = { "jִ", "ʝ" },
+	k = { "ᴋ", "ķ", "ḳ", "kׂ", "ꞣ" },
+	l = { "lề", "ḽ", "lִִִ", "ł", "ḽ", "lส" },
+	m = { "mִִִ", "ṃ", "mม", "ɱ" },
+	n = { "ň", "ภ", "и" },
+	o = { "ὂ", "ö", "ọ" },
+	p = { "ṕ", "pἷ", "pề", "ῤ" },
+	q = { "qִ", },
+	r = { "ṛ", "rִ", "ґ" },
+	s = { "ş", "şִ", "şִִִ", "ş︭", "ş︤", "şִ", "şִִ", "ş์", "ร", "ş̇" },
+	t = { "ṭ", "tִ", "ṭ︭", "ṭ︤", "tส", "т" },
+	u = { "ǘ", "ú", "ü", "ų", "บ" },
+	v = { "vִ", "v͟" },
+	w = { "ẅ", "ฬ", "ฬִ" },
+	x = { "χ", "ҳ" },
+	y = { "ÿ", "ý" },
+	z = { "zִ", "ʐ", "ž" },
+}
 
-	for word in text:gmatch("%S+") do -- jedes Wort einzeln
-		local bypassedWord = {}
-		for i = 1, #word do
-			table.insert(bypassedWord, word:sub(i, i))
-			if i < #word then
-				table.insert(bypassedWord, ".")
+-- Homoglyphen ersetzen, ohne Zero-Width
+function Bypasser.bypassText(text: string): string
+	local newText = {}
+
+	for c in text:gmatch(".") do
+		local lower = string.lower(c)
+		if charSubstitutions[lower] then
+			local subs = charSubstitutions[lower]
+			local picked = subs[math.random(1, #subs)]
+			if c == string.upper(c) then
+				table.insert(newText, string.upper(picked))
+			else
+				table.insert(newText, picked)
 			end
+		else
+			table.insert(newText, c)
 		end
-		table.insert(result, table.concat(bypassedWord))
 	end
 
-	return table.concat(result, " ")
+	return table.concat(newText)
+end
+
+-- Clipboard Support
+function Bypasser.copyToClipboard(text: string)
+	if setclipboard then
+		setclipboard(text)
+	end
 end
 
 return Bypasser
