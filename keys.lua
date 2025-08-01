602dcbd009e0b3d5f3947b7181a7044acf50b4a2
tapshift <141554865+tap-shift@users.noreply.github.com>
2025-08-01 19:17:01 +0200

    Update keys.lua

--- keys.lua
+++ keys.lua
@@ -38,7 +38,7 @@ return {
         },
         {
             key = "1234",
-            users = {"NexusNeox6", "NexusNeox60", "Player6"}
+            users = {"NexusNeox6", "NexusNeox60", "NexusNeox63"}
         },
         {
             key = "TAPX-EB-3333",
